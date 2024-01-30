1677864d83717bbbeca2de788d2ab1a248d3efd3
dimasnovik <dvn96@yandex.ru>
2024-01-31 05:00:17 +0530

    Director CRUD completed

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -15,14 +16,10 @@ import java.util.Collection;
 @RequestMapping("/films")
 @Slf4j
 @Validated
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class FilmController {
 
     private final FilmService filmService;
-
-    @Autowired
-    public FilmController(FilmService filmService) {
-        this.filmService = filmService;
-    }
 
     @GetMapping
     public Collection<Film> getAll() {
